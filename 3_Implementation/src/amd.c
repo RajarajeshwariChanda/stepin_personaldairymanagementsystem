d50fc8f8ce0d0c066c2a83f10b22588180cba640
RajarajeshwariChanda <89605184+RajarajeshwariChanda@users.noreply.github.com>
2021-09-09 01:03:07 +0530

    Add files via upload

---
+++ 3_Implementation/src/amd.c
@@ -0,0 +1,439 @@
+#include "display.h"
+
+int password()
+
+{
+
+    char pass[15]= {0},check[15]= {0},ch;
+
+    FILE *fpp;
+
+    int i=0,j;
+
+    printf("::FOR SECURITY PURPOSE::");
+
+    printf("::ONLY THREE TRIALS ARE ALLOWED::");
+
+    for(j=0; j<3; j++)
+
+    {
+
+        i=0;
+
+        printf("\n\n\tENTER THE PASSWORD:");
+
+        pass[0]=getch();
+
+        while(pass[i]!='\r')
+
+        {
+
+            if(pass[i]=='\b')
+
+            {
+
+                i--;
+
+                printf("\b");
+
+                printf(" ");
+
+                printf("\b");
+
+                pass[i]=getch();
+
+            }
+
+            else
+
+            {
+
+                printf("*");
+
+                i++;
+
+                pass[i]=getch();
+
+            }
+
+        }
+
+        pass[i]='\0';
+
+        fpp=fopen("SE","r");
+
+        if (fpp==NULL)
+
+        {
+
+            printf("\nERROR WITH THE SYSTEM FILE...[FILE MISSING]\n");
+
+            getch();
+
+            return 1;
+
+        }
+
+        else
+
+            i=0;
+
+        while(1)
+
+        {
+
+            ch=fgetc(fpp);
+
+            if(ch==EOF)
+
+            {
+
+                check[i]='\0';
+
+                break;
+
+            }
+
+            check[i]=ch-5;
+
+            i++;
+
+        }
+
+        if(strcmp(pass,check)==0)
+
+        {
+
+            printf("\n\n\tACCESS GRANTED...\n");
+
+            return 0;
+
+        }
+
+        else
+
+        {
+
+            printf("\n\n\tWRONG PASSWORD..\n\n\tACCESS DENIED...\n");
+
+        }
+
+    }
+
+    printf("\n\n\t::YOU ENTERED WRONG PASSWORD::YOU ARE NOT ALLOWED TO ACCESS ANY FILE::\n\n\tPRESS ANY KEY TO GO BACK...");
+
+    getch();
+
+    return 1;
+
+}
+
+void addrecord( )
+
+{
+
+    system("cls");
+
+    FILE *fp ;
+
+    char another = 'Y' ,time[10];
+
+    struct record e ;
+
+    char filename[15];
+
+    int choice;
+
+    printf("\n\n\t\t***************************\n");
+
+    printf("\t\t* WELCOME TO THE ADD MENU *");
+
+    printf("\n\t\t***************************\n\n");
+
+    printf("\n\n\tENTER DATE OF YOUR RECORD:[yyyy-mm-dd]:");
+
+    fflush(stdin);
+
+    gets(filename);
+
+    fp = fopen (filename, "ab+" ) ;
+
+    if ( fp == NULL )
+
+    {
+
+        fp=fopen(filename,"wb+");
+
+        if(fp==NULL)
+
+        {
+
+            printf("\nSYSTEM ERROR...");
+
+            printf("\nPRESS ANY KEY TO EXIT");
+
+            getch();
+
+            return ;
+
+        }
+
+    }
+
+    while ( another == 'Y'|| another=='y' )
+
+    {
+
+        choice=0;
+
+        fflush(stdin);
+
+        printf ( "\n\tENTER TIME:[hh:mm]:");
+
+        scanf("%s",time);
+
+        rewind(fp);
+
+        while(fread(&e,sizeof(e),1,fp)==1)
+
+        {
+
+            if(strcmp(e.time,time)==0)
+
+            {
+
+                printf("\n\tTHE RECORD ALREADY EXISTS.\n");
+
+                choice=1;
+
+            }
+
+        }
+
+        if(choice==0)
+
+        {
+
+            strcpy(e.time,time);
+
+            printf("\tENTER NAME:");
+
+            fflush(stdin);
+
+            gets(e.name);
+
+            fflush(stdin);
+
+            printf("\tENTER PLACE:");
+
+            gets(e.place);
+
+            fflush(stdin);
+
+            printf("\tENTER DURATION:");
+
+            gets(e.duration);
+
+            fflush(stdin);
+
+            printf("\tNOTE:");
+
+            gets(e.note);
+
+            fwrite ( &e, sizeof ( e ), 1, fp ) ;
+
+            printf("\nYOUR RECORD IS ADDED...\n");
+
+        }
+
+        printf ( "\n\tADD ANOTHER RECORD...(Y/N) " ) ;
+
+        fflush ( stdin ) ;
+
+        another = getchar( ) ;
+
+    }
+
+    fclose ( fp ) ;
+
+    printf("\n\n\tPRESS ANY KEY TO EXIT...");
+
+    getch();
+
+}
+void deleterecord( )
+
+{
+
+    system("cls");
+
+    FILE *fp,*fptr ;
+
+    struct record file ;
+
+    char filename[15],another = 'Y' ,time[10];;
+
+    int choice,check;
+
+    printf("\n\n\t\t*************************\n");
+
+    printf("\t\t* WELCOME TO DELETE MENU*");
+
+    printf("\n\t\t*************************\n\n");
+
+    check = password();
+
+    if(check==1)
+
+    {
+
+        return ;
+
+    }
+
+    while ( another == 'Y' )
+
+    {
+
+        printf("\n\n\tHOW WOULD YOU LIKE TO DELETE.");
+
+        printf("\n\n\t#DELETE WHOLE RECORD\t\t\t[1]");
+
+        printf("\n\t#DELETE A PARTICULAR RECORD BY TIME\t[2]");
+
+        do
+
+        {
+
+            printf("\n\t\tENTER YOU CHOICE:");
+
+            scanf("%d",&choice);
+
+            switch(choice)
+
+            {
+
+            case 1:
+
+                printf("\n\tENTER THE DATE OF RECORD TO BE DELETED:[yyyy-mm-dd]:");
+
+                fflush(stdin);
+
+                gets(filename);
+
+                fp = fopen (filename, "wb" ) ;
+
+                if ( fp == NULL )
+
+                {
+
+                    printf("\nTHE FILE DOES NOT EXISTS");
+
+                    printf("\nPRESS ANY KEY TO GO BACK.");
+
+                    getch();
+
+                    return ;
+
+                }
+
+                fclose(fp);
+
+                remove(filename);
+
+                printf("\nDELETED SUCCESFULLY...");
+
+                break;
+
+            case 2:
+
+                printf("\n\tENTER THE DATE OF RECORD:[yyyy-mm-dd]:");
+
+                fflush(stdin);
+
+                gets(filename);
+
+                fp = fopen (filename, "rb" ) ;
+
+                if ( fp == NULL )
+
+                {
+
+                    printf("\nTHE FILE DOES NOT EXISTS");
+
+                    printf("\nPRESS ANY KEY TO GO BACK.");
+
+                    getch();
+
+                    return ;
+
+                }
+
+                fptr=fopen("temp","wb");
+
+                if(fptr==NULL)
+
+                {
+
+                    printf("\nSYSTEM ERROR");
+
+                    printf("\nPRESS ANY KEY TO GO BACK");
+
+                    getch();
+
+                    return ;
+
+                }
+
+                printf("\n\tENTER THE TIME OF RECORD TO BE DELETED:[hh:mm]:");
+
+                fflush(stdin);
+
+                gets(time);
+
+                while(fread(&file,sizeof(file),1,fp)==1)
+
+                {
+
+                    if(strcmp(file.time,time)!=0)
+
+                        fwrite(&file,sizeof(file),1,fptr);
+
+                }
+
+                fclose(fp);
+
+                fclose(fptr);
+
+                remove(filename);
+
+                rename("temp",filename);
+
+                printf("\nDELETED SUCCESFULLY...");
+
+                break;
+
+            default:
+
+                printf("\n\tYOU ENTERED WRONG CHOICE");
+
+                break;
+
+            }
+
+        }
+        while(choice<1||choice>2);
+
+        printf("\n\tDO YOU LIKE TO DELETE ANOTHER RECORD.(Y/N):");
+
+        fflush(stdin);
+
+        scanf("%c",&another);
+
+    }
+
+    printf("\n\n\tPRESS ANY KEY TO EXIT...");
+
+    getch();
+
+}
